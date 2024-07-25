2d486783e3a120b72cc84d5e7e377520074cfccf
thanhtruc350 <23130350@st.hcmuaf.edu.vn>
2024-07-25 14:02:38 +0700

    Giao dien vua duoc update

--- src/myPaint/ScrollPane.java
+++ src/myPaint/ScrollPane.java
@@ -40,8 +40,12 @@ public class ScrollPane extends JPanel {
                 }
             }
         }
-
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(800, 600);
+        }
     }
+
 
 
 }
