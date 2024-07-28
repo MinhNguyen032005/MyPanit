1a8a3c7f9abc4ac1aeb128f87dd930ac21ef3f9d
Duy <duynguyenavg@gmail.com>
2024-07-28 23:29:13 +0700

    Thêm chức năng lưu hình  # chưa test được chính xác đã lưu đc hay chưa do lỗi pane để hiện hình vẽ đã fix có thêm chức năng ko cho lưu khi ko có hình vẽ (đã có demo ở 1 lớp riêng bên ngoài project chạy  thử hàm lưu hình thì đã lưu đc hình)  # Bạn nào Code phần vẽ hình thì pull code về code tiếp chạy thử xem hàm lưu hình có lỗi ko nếu lỗi nói tui fix.

--- src/ourTeam/MyFrame.java
+++ src/ourTeam/MyFrame.java
@@ -3,24 +3,34 @@ package ourTeam;
 
 
 import controller.ActionWhenPaint;
+import controller.IController;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
 
 public class MyFrame extends JFrame {
     private MyMainPanel mainPanel;
-    public MyFrame(MyMainPanel mainPanel) {
+    private ActionWhenPaint actionWhenPaint;
+
+    public MyFrame() {
         setTitle("Green Paint");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(900, 550);
         setLayout(new BorderLayout());
         setLocationRelativeTo(null);
-
+        actionWhenPaint = new ActionWhenPaint();
+        mainPanel = new MyMainPanel(actionWhenPaint);
 
         MyMenuBar menuBar = new MyMenuBar();
-       this.mainPanel = mainPanel;
 
-//
+        JPanel drawPanel = mainPanel.getPanelPaint().getCustomPanel();
+        menuBar.setDrawingPanel(drawPanel);
+
+        this.mainPanel = mainPanel;
+
+
 
         this.setJMenuBar(menuBar);
         getContentPane().add(mainPanel);
@@ -29,7 +39,7 @@ public class MyFrame extends JFrame {
     }
 
     public static void main(String[] args) {
-        new MyFrame(new MyMainPanel(new ActionWhenPaint()));
+        new MyFrame();
 
 
     }
--- src/ourTeam/MyMainPanel.java
+++ src/ourTeam/MyMainPanel.java
@@ -7,20 +7,24 @@ import java.awt.*;
 
 public class MyMainPanel extends JPanel {
     IController iController;
+    MyPanelPaint panelPaint;
     public MyMainPanel(IController controller) {
-        this.iController= controller;
+        this.iController = controller;
         this.setLayout(new BorderLayout());
         MyPanelTop panelTop = new MyPanelTop(iController);
         MyPanelLeft panelLeft = new MyPanelLeft(iController);
         MyPanelBot panelBot = new MyPanelBot();
-        MyPanelPaint panelPaint = new MyPanelPaint(iController);
+        panelPaint = new MyPanelPaint(iController);
 //
         this.add(panelTop, BorderLayout.NORTH);
         this.add(panelLeft, BorderLayout.WEST);
         this.add(panelBot, BorderLayout.SOUTH);
         this.add(panelPaint, BorderLayout.CENTER);
-
-
-
     }
+
+    public MyPanelPaint getPanelPaint() {
+        return panelPaint;
+    }
+
+
 }
